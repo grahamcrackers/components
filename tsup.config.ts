851884fb3e26d88ad46bc5fe6a900fef80d58d8c
Graham Rogers <gmrogers04@gmail.com>
2024-06-25 16:04:12 -0600

    fix: fixing build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,29 @@
-import type { Options } from "tsup";
+// import type { Options } from "tsup";
 
-export const tsup: Options = {
-    entry: ["src/**/*.ts"],
-    treeshake: true,
-    sourcemap: false,
-    minify: true,
-    clean: true,
-    dts: true,
-    splitting: false,
+// export const tsup: Options = {
+//     entry: ["src/**/*.ts"],
+//     treeshake: true,
+//     sourcemap: false,
+//     minify: true,
+//     clean: true,
+//     dts: true,
+//     splitting: false,
+//     target: "es2020",
+//     format: ["cjs", "esm"],
+//     external: ["react", "react/jsx-runtime"],
+//     injectStyle: false,
+// };
+
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+    entry: ["src/index.ts", "src/**/*.ts", "src/**/*.tsx"],
     format: ["cjs", "esm"],
-    external: ["react", "react-dom", "react/jsx-runtime"],
-    injectStyle: false,
-};
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    target: "es2022",
+    external: ["react", "react/jsx-runtime"],
+    minify: !options.watch,
+    banner: { js: '"use client";' },
+}));
