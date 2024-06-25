77f4cc183347b9072e5250f764a1112fe7ac2844
Graham Rogers <gmrogers04@gmail.com>
2024-06-25 09:21:28 -0600

    adding scripts to adjust package.json

---
+++ scripts/build.ts
@@ -0,0 +1,7 @@
+import { exec } from "child_process";
+
+(async () => {
+    await exec("yarn tsup && cp ./src/index.css ./dist cp ./tailwind.config.ts ./dist");
+    await exec("cp package.json pkg.json")
+})()
+
---
+++ scripts/exports.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+import pkg from "../package.json";
+
+const SRC = "./src/components";
+
+(async () => {
+    const allFiles = fs.readdirSync(SRC, { recursive: true }) as string[];
+    const filtered = allFiles.map(file => {
+        const parts = file.split("/");
+        if(parts.length > 1) return parts[1]
+    }).filter(file => file !== "index.ts" && file);
+
+    // mapped exports
+    const files = filtered.reduce((acc, cur) => {
+        const componentName = cur?.split(".")[0];
+        return { 
+            ...acc,
+            [`./${componentName}`]: {
+            "require": `./dist/components/${componentName}/index.cjs`,
+            "import": `./dist/components/${componentName}/index.js`,
+            "types": `./dist/components/${componentName}/index.d.ts`
+        }}
+    }, {})
+    
+    pkg.exports = {
+        ...pkg.exports,
+        ...files
+    }
+})()
