053c907612ba4fc05ba8ea2a92a6e9e356128060
Graham Rogers <gmrogers04@gmail.com>
2024-06-25 12:12:02 -0600

    chore: release v0.0.9

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,5 +2,4 @@ import { exec } from "child_process";
 
 (async () => {
     await exec("yarn tsup && cp ./src/index.css ./dist cp ./tailwind.config.ts ./dist");
-    await exec("cp package.json pkg.json");
 })();
